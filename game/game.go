6576110ac52f01d49e6cad3ed8289fa8e2eb395a
Abimael Martinez <abimael.martinez@mail.foxconn.com>
2015-03-03 16:53:40 -0500

    Implementation of play

--- game/game.go
+++ game/game.go
@@ -5,10 +5,34 @@ type Player interface {
 	Play(chan [][]bool)
 }
 
-type Naive struct {
+type Simple struct {
 	world [][]bool
 }
 
-func New([][]bool) Player {
+func iterate(current [][]bool) [][]bool {
+	xSize, ySize := len(current[0]), len(current)
+	nextgen := make([][]bool, ySize)
 
+	for y, row := range current {
+		nextgen[y] = make([]bool, xSize)
+		for x := range row {
+			nextgen[y][x] = isAlive(cell{x, y}, current)
+		}
+	}
+
+	return nextgen
+}
+
+func (s Simple) Play(c chan [][]bool) {
+	world := s.world
+
+	for {
+		world = iterate(world)
+		c <- world
+
+	}
+}
+
+func New(w [][]bool) Player {
+	return Simple{w}
 }
