a30bcd99e169b4382a02231472a06bcf9e542627
Abimael Martinez <abimael.martinez@mail.foxconn.com>
2015-03-05 09:42:01 -0500

    Add game server

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/abijr/GameOfLife/game"
+)
+
+func main() {
+	log.Println("Running webapp at http://localhost:8080")
+	http.Handle("/", http.FileServer(http.Dir("./static/dist/")))
+	http.HandleFunc("/play", play)
+	http.ListenAndServe(":8080", nil)
+}
+
+func play(w http.ResponseWriter, r *http.Request) {
+	var d [][]bool
+	defer r.Body.Close()
+	dec := json.NewDecoder(r.Body)
+
+	err := dec.Decode(&d)
+	if err != nil {
+		log.Println("Err: ", err)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.Header().Add("Access-Control-Allow-Origin", "localhost")
+
+	g := game.New(d)
+	done := make(chan struct{})
+	results := g.Play(done)
+	s := <-results
+
+	data, err := json.Marshal(s.World())
+	fmt.Fprint(w, string(data))
+}
